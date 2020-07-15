3c225b97aa635a408777cf849f2d51068f4cd802
DomainFlag <Cchivriga@hotmail.com>
2020-07-15 17:20:15 +0200

    added hierarchical mesh sampling, unable to perform poisson reconstruction as the density spans higher length

--- src/mesh_nerf.py
+++ src/mesh_nerf.py
@@ -1,13 +1,10 @@
 import argparse
-import os
-import time
 import numpy as np
 import torch
 import torchvision
 import yaml
 
-from pathlib import Path
-from nerf import run_network
+from nerf.train_utils import predict_and_render_radiance
 from skimage import measure
 from scipy.spatial import KDTree
 from tqdm import tqdm
@@ -16,23 +13,28 @@ from nerf import get_minibatches
 
 from nerf import (
     CfgNode,
+    get_ray_bundle,
     load_blender_data,
     load_llff_data,
     models,
     get_embedding_function,
-    predict_and_render_radiance,
-    volume_render_radiance_field
+    run_one_iter_of_nerf,
 )
 
 def export_obj(vertices, triangles, diffuse, normals, filename):
     """
     Exports a mesh in the (.obj) format.
     """
+    print('Writing to obj')
 
     with open(filename, 'w') as fh:
 
         for index, v in enumerate(vertices):
-            fh.write("v {} {} {} {} {} {}\n".format(*v, *diffuse[index]))
+            fh.write("v {} {} {}".format(*v))
+            if len(diffuse) > index:
+                fh.write(" {} {} {}".format(*diffuse[index]))
+
+            fh.write("\n")
 
         for n in normals:
             fh.write("vn {} {} {}\n".format(*n))
@@ -44,94 +46,11 @@ def export_obj(vertices, triangles, diffuse, normals, filename):
 
             fh.write("\n")
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--config", type = str, required = True, help = "Path to (.yml) config file."
-    )
-    parser.add_argument(
-        "--base-dir",
-        type = str,
-        required = False,
-        help = "Override the default base dir.",
-    )
-    parser.add_argument(
-        "--checkpoint",
-        type = str,
-        required = True,
-        help = "Checkpoint / pre-trained model to evaluate.",
-    )
-    parser.add_argument(
-        "--save-dir", type = str, help = "Save mesh to this directory, if specified."
-    )
 
-    parser.add_argument('--cache-mesh', dest = 'cache_mesh', action = 'store_true')
-    parser.add_argument('--no-cache-mesh', dest = 'cache_mesh', action = 'store_false')
-    parser.set_defaults(cache_mesh = True)
-
-    configargs = parser.parse_args()
-
-    # Read config file.
-    cfg, model_name = None, None
-    with open(configargs.config, "r") as f:
-        cfg_dict = yaml.load(f, Loader = yaml.FullLoader)
-        cfg, model_name = CfgNode(cfg_dict), Path(f.name).stem
-
-    # Device on which to run.
-    device = "cpu" if not torch.cuda.is_available() else "cuda"
-
-    encode_position_fn = get_embedding_function(
-        num_encoding_functions = cfg.models.coarse.num_encoding_fn_xyz,
-        include_input = cfg.models.coarse.include_input_xyz,
-        log_sampling = cfg.models.coarse.log_sampling_xyz,
-    )
-
-    encode_direction_fn = None
-    if cfg.models.coarse.use_viewdirs:
-        encode_direction_fn = get_embedding_function(
-            num_encoding_functions = cfg.models.coarse.num_encoding_fn_dir,
-            include_input = cfg.models.coarse.include_input_dir,
-            log_sampling = cfg.models.coarse.log_sampling_dir,
-        )
-
-    # Initialize a coarse resolution model.
-    model_coarse = getattr(models, cfg.models.coarse.type)(
-        num_encoding_fn_xyz = cfg.models.coarse.num_encoding_fn_xyz,
-        num_encoding_fn_dir = cfg.models.coarse.num_encoding_fn_dir,
-        include_input_xyz = cfg.models.coarse.include_input_xyz,
-        include_input_dir = cfg.models.coarse.include_input_dir,
-        use_viewdirs = cfg.models.coarse.use_viewdirs,
-    ).to(device)
-
-    # If a fine-resolution model is specified, initialize it.
-    model_fine = None
-    if hasattr(cfg.models, "fine"):
-        model_fine = getattr(models, cfg.models.fine.type)(
-            num_encoding_fn_xyz = cfg.models.fine.num_encoding_fn_xyz,
-            num_encoding_fn_dir = cfg.models.fine.num_encoding_fn_dir,
-            include_input_xyz = cfg.models.fine.include_input_xyz,
-            include_input_dir = cfg.models.fine.include_input_dir,
-            use_viewdirs = cfg.models.fine.use_viewdirs,
-        ).to(device)
-
-    checkpoint = torch.load(configargs.checkpoint)
-    model_coarse.load_state_dict(checkpoint["model_coarse_state_dict"])
-    if checkpoint["model_fine_state_dict"]:
-        try:
-            model_fine.load_state_dict(checkpoint["model_fine_state_dict"])
-        except:
-            print(
-                "The checkpoint has a fine-level model, but it could "
-                "not be loaded (possibly due to a mismatched config file."
-            )
-
-    model_coarse.eval()
-    if model_fine:
-        model_fine.eval()
-
+def export_marching_cubes(model_coarse, model_fine, config_args, cfg, encode_position_fn, encode_direction_fn, device):
     # Mesh Extraction
-    N = 128
-    iso_value = 32
+    N = 256
+    iso_value = config_args.iso_level
     batch_size = 1024
     density_samples_count = 6
     chunk = int(density_samples_count / 2)
@@ -141,13 +60,13 @@ def main():
     limit = 1.2
     t = np.linspace(-limit, limit, N)
     sampling_method = 0
-    adjust_normals = False
     specific_view = False
+    adjust_normals = False
     plane_near = 0
-    plane_far = 6
+    plane_far = 4
 
     vertices, triangles, normals, diffuse = None, None, None, None
-    if configargs.cache_mesh:
+    if config_args.cache_mesh:
         query_pts = np.stack(np.meshgrid(t, t, t), -1).astype(np.float32)
         pts = torch.from_numpy(query_pts)
         dimension = pts.shape[-1]
@@ -171,6 +90,8 @@ def main():
 
         # Create a 3D density grid
         grid_alpha = density.reshape((N, N, N))
+        print(f"Max density: {grid_alpha.max()}, Min density {grid_alpha.min()}, Mean density {grid_alpha.mean()}")
+        print(f"Querying based on iso level: {iso_value}")
 
         # Extracting iso-surface triangulated
         vertices, triangles, normals, values = measure.marching_cubes(grid_alpha, iso_value)
@@ -193,6 +114,7 @@ def main():
                     embedded_dirs = encode_direction_fn(batch)
                     embedded = torch.cat((embedded, embedded_dirs), dim = -1)
 
+                # result_batch = model_fine(embedded)
                 result_batch = model_fine(embedded)
 
                 # Query the whole diffuse map
@@ -247,10 +169,10 @@ def main():
                 if sample_density_1 < sample_density_2:
                     normals[index] *= (-1)
 
-        np.save(os.path.join(configargs.save_dir, "mesh_cache.npy"), (vertices, triangles, normals))
+        np.save("mesh_sample.npy", (vertices, triangles, normals))
         print("Saved successfully")
     else:
-        vertices, triangles, normals = np.load(os.path.join(configargs.save_dir, "mesh_cache.npy"), allow_pickle = True)
+        vertices, triangles, normals = np.load("mesh_sample.npy", allow_pickle = True)
 
     # Extracting the diffuse color
     # Ray targets
@@ -269,7 +191,7 @@ def main():
     ray_origins = targets + view_disparity * directions
 
     # Ray directions
-    ray_directions_loose = targets  - ray_origins
+    ray_directions_loose = targets - ray_origins
     ray_directions = ray_directions_loose / ray_directions_loose.norm(dim = 1).unsqueeze(1)
 
     near = plane_near * torch.ones_like(ray_directions[..., :1])
@@ -285,23 +207,201 @@ def main():
     ray_batches = get_minibatches(rays, chunksize = 2048)
 
     pred = []
-    for ray_batch in ray_batches:
+    for ray_batch in tqdm(ray_batches):
         # move to appropriate device
         ray_batch = ray_batch.to(device)
 
-        _, _, _, diffuse, _, _ = predict_and_render_radiance(ray_batch, model_coarse, model_fine, cfg,
+        diffuse_coarse, _, _, _, diffuse_fine, _, _, _ = predict_and_render_radiance(ray_batch, model_coarse, model_fine, cfg,
                 mode = "validation",
                 encode_position_fn = encode_position_fn,
                 encode_direction_fn = encode_direction_fn,
         )
 
-        pred.append(diffuse.cpu().detach())
+        pred.append(diffuse_fine.cpu().detach())
 
     # Query the whole diffuse map
-    diffuse = torch.cat(pred, dim = 0).numpy()
+    diffuse_fine = torch.cat(pred, dim = 0).numpy()
 
     # Export model
-    export_obj(vertices, triangles, diffuse, normals, os.path.join(configargs.save_dir, f"{model_name}.obj"))
+    export_obj(vertices, triangles, diffuse_fine, normals, "lego.obj")
+
+
+def density_rec_query(model_fine, encode_position_fn, encode_direction_fn, offset, spacing, samples = 128, min_iso_level = 0):
+    batch_size = 512
+    t = np.linspace(0, 1, samples)
+
+    leap = spacing / (samples - 1)
+
+    query_pts = (np.stack(np.meshgrid(t, t, t), -1).swapaxes(1, 0) * spacing + offset).astype(np.float32)
+    pts = torch.from_numpy(query_pts)
+    dimension = pts.shape[-1]
+
+    pts_flat = pts.reshape((-1, dimension))
+    pts_flat_batch = pts_flat.reshape((-1, batch_size, dimension))
+
+    density = np.zeros((pts_flat.shape[0]))
+    color_map = np.zeros((pts_flat.shape[0], 3))
+    for idx, batch in enumerate(pts_flat_batch):
+        batch = batch.cuda()
+
+        embedded = encode_position_fn(batch)
+        if encode_direction_fn is not None:
+            embedded_dirs = encode_direction_fn(batch)
+            embedded = torch.cat((embedded, embedded_dirs), dim = -1)
+
+        result_batch = model_fine(embedded)
+
+        # Extracting the density
+        density[idx * batch_size: (idx + 1) * batch_size] = result_batch[..., 3].cpu().detach().numpy()
+        color_map[idx * batch_size: (idx + 1) * batch_size, :] = torch.sigmoid(result_batch[..., :3]).cpu().detach().numpy()
+
+    # Create a 3D density grid
+    grid_alpha = density.reshape((samples, samples, samples))
+    grid_mean = grid_alpha.mean()
+    iso_value = max(grid_mean, min_iso_level)
+
+    use_verbose = False
+    if use_verbose:
+        print(f"Max density: {grid_alpha.max()}, Min density {grid_alpha.min()}, Mean density {grid_mean}")
+        print(f"Querying based on iso level: {iso_value}")
+
+    N = grid_alpha.shape[0]
+    mask = grid_alpha >= iso_value
+    voxels = np.argwhere(mask) / (N - 1) * spacing + offset - leap / 2
+
+    return voxels, iso_value, leap, color_map[mask.reshape(-1), :]
+
+
+def export_poisson(model_coarse, model_fine, config_args, cfg, encode_position_fn, encode_direction_fn, device):
+    # Mesh Extraction
+    N = 64
+    limit = 1.2
+    t = np.linspace(-limit, limit, N)
+    min_iso_level = 64
+
+    offset = 2 * limit / (N - 1)
+    assert(abs(abs(t[0] - t[1]) - offset) < 1e-8)
+
+    voxels, iso_level, leap, _ = density_rec_query(model_fine, encode_position_fn, encode_direction_fn, -limit, 2 * limit, samples = N, min_iso_level = min_iso_level)
+    print(f"Total Voxels: {N ** 3} Inliers: {len(voxels)} Percentage: {(len(voxels) / N ** 3 * 100):.2f}%")
+
+    vertices = []
+    colors = []
+    for voxel in tqdm(voxels):
+        voxel_inner, _, _, color_map = density_rec_query(model_fine, encode_position_fn, encode_direction_fn, voxel, leap, samples = 8, min_iso_level = iso_level)
+
+        vertices.append(voxel_inner)
+        colors.append(color_map)
+
+    vertices = np.concatenate(vertices, axis = 0)
+    colors = np.concatenate(colors, axis = 0)
+
+    print(f"Total vertices {len(vertices)}")
+    export_obj(vertices, [], colors, [], "lego_poisson.obj")
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--config", type = str, required = True, help = "Path to (.yml) config file."
+    )
+    parser.add_argument(
+        "--base-dir",
+        type = str,
+        required = False,
+        help = "Override the default base dir.",
+    )
+    parser.add_argument(
+        "--checkpoint",
+        type = str,
+        required = True,
+        help = "Checkpoint / pre-trained model to evaluate.",
+    )
+    parser.add_argument(
+        "--save-dir", type = str, help = "Save mesh to this directory, if specified."
+    )
+    
+    parser.add_argument(
+        "--iso-level",
+        type = float,
+        help = "Iso-Level to be queried",
+        default = 32
+    )
+
+    parser.add_argument('--cache-mesh', dest = 'cache_mesh', action = 'store_true')
+    parser.add_argument('--no-cache-mesh', dest = 'cache_mesh', action = 'store_false')
+    parser.set_defaults(cache_mesh = True)
+
+    config_args = parser.parse_args()
+
+    # Read config file.
+    cfg = None
+    with open(config_args.config, "r") as f:
+        cfg_dict = yaml.load(f, Loader = yaml.FullLoader)
+        cfg = CfgNode(cfg_dict)
+
+    # Device on which to run.
+    device = "cpu"
+    if torch.cuda.is_available():
+        device = "cuda"
+
+    encode_position_fn = get_embedding_function(
+        num_encoding_functions = cfg.models.coarse.num_encoding_fn_xyz,
+        include_input = cfg.models.coarse.include_input_xyz,
+        log_sampling = cfg.models.coarse.log_sampling_xyz,
+    )
+
+    encode_direction_fn = None
+    if cfg.models.coarse.use_viewdirs:
+        encode_direction_fn = get_embedding_function(
+            num_encoding_functions = cfg.models.coarse.num_encoding_fn_dir,
+            include_input = cfg.models.coarse.include_input_dir,
+            log_sampling = cfg.models.coarse.log_sampling_dir,
+        )
+
+    # Initialize a coarse resolution model.
+    model_coarse = getattr(models, cfg.models.coarse.type)(
+        num_encoding_fn_xyz = cfg.models.coarse.num_encoding_fn_xyz,
+        num_encoding_fn_dir = cfg.models.coarse.num_encoding_fn_dir,
+        include_input_xyz = cfg.models.coarse.include_input_xyz,
+        include_input_dir = cfg.models.coarse.include_input_dir,
+        use_viewdirs = cfg.models.coarse.use_viewdirs,
+    )
+    model_coarse.to(device)
+
+    # If a fine-resolution model is specified, initialize it.
+    model_fine = None
+    if hasattr(cfg.models, "fine"):
+        model_fine = getattr(models, cfg.models.fine.type)(
+            num_encoding_fn_xyz = cfg.models.fine.num_encoding_fn_xyz,
+            num_encoding_fn_dir = cfg.models.fine.num_encoding_fn_dir,
+            include_input_xyz = cfg.models.fine.include_input_xyz,
+            include_input_dir = cfg.models.fine.include_input_dir,
+            use_viewdirs = cfg.models.fine.use_viewdirs,
+        )
+        model_fine.to(device)
+
+    checkpoint = torch.load(config_args.checkpoint)
+    model_coarse.load_state_dict(checkpoint["model_coarse_state_dict"])
+    if checkpoint["model_fine_state_dict"]:
+        try:
+            model_fine.load_state_dict(checkpoint["model_fine_state_dict"])
+        except:
+            print(
+                "The checkpoint has a fine-level model, but it could "
+                "not be loaded (possibly due to a mismatched config file."
+            )
+
+    model_coarse.eval()
+    if model_fine:
+        model_fine.eval()
+
+    use_marching_cubes = False
+    if use_marching_cubes:
+        export_marching_cubes(model_coarse, model_fine, config_args, cfg, encode_position_fn, encode_direction_fn, device)
+    else:
+        export_poisson(model_coarse, model_fine, config_args, cfg, encode_position_fn, encode_direction_fn, device)
+
 
 if __name__ == "__main__":
     main()
